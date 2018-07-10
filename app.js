67c122ba508870af8d4bd516392d123e89348e17
PetyuskaDoom <patzer1941@gmail.com>
2018-07-10 17:02:01 +0100

    Bug fixes1

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //console.log("TEST");
+//  Use nodemon so we don't have to stop and restart the server every time a change is made.
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -11,9 +12,8 @@ var port = 3000;
 // // Middleware, outputs to console everytime page is refreshed
 // var logger = function(req, res, next) {
 //   console.log('logging...');
-//   next();
+//   next(); // This ends this piece of middleware and calls the next middleware function.
 // }
-
 // app.use(logger);
 
 // Middleware view engine
@@ -26,14 +26,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Set static path
+// Set static path (Any static resources go into the public folder - HTML, CSS etc)
+// The contents of public will overwrite anything defined in app.js
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Global vars
-app.use(function(req, res, next) {
-  res.locals.errors = null;
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.locals.errors = null;
+//   next();
+// });
 
 // var people = [
 //   {
@@ -50,7 +51,7 @@ app.use(function(req, res, next) {
 //   }
 // ];
 
-// Express validator middleware
+// Express validator middleware (these are functions that have access to request and response objects)
 // app.use(expressValidator({
 //   errorFormatter: function(param, msg, value) {
 //     var namespace = param.split('.'),
@@ -93,12 +94,13 @@ var users = [
   }
 ];
 
-// Use an anonymous callback function as second param.
+// Route handler. Use an anonymous callback function as second param.
 app.get('/', function(req, res) {
-  //res.send('TESTING 10'); // send method prints out to browser screen the agrument.
-  //res.json(people);
+  // res.send('TESTING 12'); // send method prints out to browser screen the agrument.
+  // res.json(people);
   
-  //var title = 'Customers';
+  // var title = 'Customers';
+  // res.render('index');
   res.render('index', {
     title: 'Customers',
     users: users
@@ -106,36 +108,36 @@ app.get('/', function(req, res) {
 });
 
 app.post('/users/add', function(req, res) {
-  //console.log("FORM SUBMITTED"); // Outputs to command window.
-  //console.log(req.body.firstName);
+  console.log("FORM SUBMITTED"); // Outputs to command window.
+  // console.log(req.body.first_name);
 
-  req.checkBody('firstName', 'First Name is required').notEmpty();
-  req.checkBody('lastName', 'Last Name is required').notEmpty();
-  req.checkBody('email', 'Email is required').notEmpty();
+  // console.log(req.checkBody('first_name', 'First Name is required').notEmpty());
+//   req.checkBody('lastName', 'Last Name is required').notEmpty();
+//   req.checkBody('email', 'Email is required').notEmpty();
 
-  var errors = req.validationErrors();
+//   var errors = req.validationErrors();
 
-  if(errors) {
-    console.log("ERRORS!!!");
-    res.render('index', {
-      title: 'Customers',
-      users: users,
-      errors: errors
-    });
-  }
-  else {
+//   if(errors) {
+//     console.log("ERRORS!!!");
+//     res.render('index', {
+//       title: 'Customers',
+//       users: users,
+//       errors: errors
+//     });
+//   }
+//   else {
     var newUser = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
       email: req.body.email
     }
     console.log(newUser);
     console.log('SUCCESS!!!');
-  }
+//   }
 });
 
 app.listen(port, function() {
-  console.log("Server started on port " + port);
+  console.log(`Server started on port ${port}`);
 });
 
 // Install nodemon to monitor for any changes node.js (app.js) application and automatically restart the server when a page is refreshed with new data. No need to press Ctrl + c and type node app every time a change is made.
